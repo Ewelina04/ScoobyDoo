58c474df19d7b8b7714d27a59ca359c345b2c7a9
Ewelina04 <50328193+Ewelina04@users.noreply.github.com>
2020-01-28 13:04:16 +0100

    Update 02.py

--- 02.py
+++ 02.py
@@ -4,7 +4,7 @@ from psychopy import visual, event, core
 import pandas as pd
 import filterlib as flt
 import blink as blk
-#from pyOpenBCI import OpenBCIGanglion
+from pyOpenBCI import OpenBCIGanglion
 
 import pygame
 from pygame.locals import *
